f118fe6f6b163d07910976784f5635b2c59e2fff
Vy Nghia <dora.nghia@gmail.com>
2018-04-14 12:57:42 +0700

    Add files via upload

---
+++ Convert to Anime/Main.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Convert_to_Anime
+{
+    partial class ConvertToAnime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imageAddress = new System.Windows.Forms.TextBox();
+            this.selectImageButton = new System.Windows.Forms.Button();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.imageAddressLabel = new System.Windows.Forms.Label();
+            this.saveLabel = new System.Windows.Forms.Label();
+            this.saveAddress = new System.Windows.Forms.TextBox();
+            this.selectFolderButton = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.Label();
+            this.CopyrightLabel = new System.Windows.Forms.Label();
+            this.typeConvert1 = new System.Windows.Forms.RadioButton();
+            this.typeConvert2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // imageAddress
+            // 
+            this.imageAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.imageAddress.Location = new System.Drawing.Point(133, 53);
+            this.imageAddress.Name = "imageAddress";
+            this.imageAddress.ReadOnly = true;
+            this.imageAddress.Size = new System.Drawing.Size(333, 26);
+            this.imageAddress.TabIndex = 0;
+            // 
+            // selectImageButton
+            // 
+            this.selectImageButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectImageButton.Location = new System.Drawing.Point(472, 54);
+            this.selectImageButton.Name = "selectImageButton";
+            this.selectImageButton.Size = new System.Drawing.Size(108, 26);
+            this.selectImageButton.TabIndex = 1;
+            this.selectImageButton.Text = "Select image...";
+            this.selectImageButton.UseVisualStyleBackColor = true;
+            this.selectImageButton.Click += new System.EventHandler(this.selectImage);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Enabled = false;
+            this.convertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertButton.Location = new System.Drawing.Point(586, 54);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(82, 25);
+            this.convertButton.TabIndex = 2;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertImage);
+            // 
+            // imageAddressLabel
+            // 
+            this.imageAddressLabel.AutoSize = true;
+            this.imageAddressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.imageAddressLabel.Location = new System.Drawing.Point(12, 56);
+            this.imageAddressLabel.Name = "imageAddressLabel";
+            this.imageAddressLabel.Size = new System.Drawing.Size(115, 20);
+            this.imageAddressLabel.TabIndex = 4;
+            this.imageAddressLabel.Text = "Image address";
+            // 
+            // saveLabel
+            // 
+            this.saveLabel.AutoSize = true;
+            this.saveLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveLabel.Location = new System.Drawing.Point(12, 15);
+            this.saveLabel.Name = "saveLabel";
+            this.saveLabel.Size = new System.Drawing.Size(63, 20);
+            this.saveLabel.TabIndex = 5;
+            this.saveLabel.Text = "Save to";
+            // 
+            // saveAddress
+            // 
+            this.saveAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveAddress.Location = new System.Drawing.Point(133, 12);
+            this.saveAddress.Name = "saveAddress";
+            this.saveAddress.ReadOnly = true;
+            this.saveAddress.Size = new System.Drawing.Size(333, 26);
+            this.saveAddress.TabIndex = 6;
+            this.saveAddress.Text = "C:\\";
+            // 
+            // selectFolderButton
+            // 
+            this.selectFolderButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectFolderButton.Location = new System.Drawing.Point(472, 12);
+            this.selectFolderButton.Name = "selectFolderButton";
+            this.selectFolderButton.Size = new System.Drawing.Size(108, 29);
+            this.selectFolderButton.TabIndex = 7;
+            this.selectFolderButton.Text = "Select folder...";
+            this.selectFolderButton.UseVisualStyleBackColor = true;
+            this.selectFolderButton.Click += new System.EventHandler(this.selectSaveFolder);
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Status.Location = new System.Drawing.Point(12, 113);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(229, 16);
+            this.Status.TabIndex = 8;
+            this.Status.Text = "Trạng thái: Vui lòng chọn file hình ảnh!";
+            // 
+            // CopyrightLabel
+            // 
+            this.CopyrightLabel.AutoSize = true;
+            this.CopyrightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CopyrightLabel.Location = new System.Drawing.Point(562, 113);
+            this.CopyrightLabel.Name = "CopyrightLabel";
+            this.CopyrightLabel.Size = new System.Drawing.Size(105, 16);
+            this.CopyrightLabel.TabIndex = 9;
+            this.CopyrightLabel.Text = "© 2018 Vy Nghia";
+            this.CopyrightLabel.Click += new System.EventHandler(this.copyrightAuthor);
+            // 
+            // typeConvert1
+            // 
+            this.typeConvert1.AutoSize = true;
+            this.typeConvert1.Checked = true;
+            this.typeConvert1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.typeConvert1.Location = new System.Drawing.Point(16, 90);
+            this.typeConvert1.Name = "typeConvert1";
+            this.typeConvert1.Size = new System.Drawing.Size(87, 20);
+            this.typeConvert1.TabIndex = 10;
+            this.typeConvert1.TabStop = true;
+            this.typeConvert1.Tag = "";
+            this.typeConvert1.Text = "Sáng sớm";
+            this.typeConvert1.UseVisualStyleBackColor = true;
+            // 
+            // typeConvert2
+            // 
+            this.typeConvert2.AutoSize = true;
+            this.typeConvert2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.typeConvert2.Location = new System.Drawing.Point(109, 90);
+            this.typeConvert2.Name = "typeConvert2";
+            this.typeConvert2.Size = new System.Drawing.Size(74, 20);
+            this.typeConvert2.TabIndex = 11;
+            this.typeConvert2.Text = "Chiều tà";
+            this.typeConvert2.UseVisualStyleBackColor = true;
+            // 
+            // ConvertToAnime
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 138);
+            this.Controls.Add(this.typeConvert2);
+            this.Controls.Add(this.typeConvert1);
+            this.Controls.Add(this.CopyrightLabel);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.selectFolderButton);
+            this.Controls.Add(this.saveAddress);
+            this.Controls.Add(this.saveLabel);
+            this.Controls.Add(this.imageAddressLabel);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.selectImageButton);
+            this.Controls.Add(this.imageAddress);
+            this.MaximizeBox = false;
+            this.Name = "ConvertToAnime";
+            this.Text = "Convert to Anime";
+            this.Load += new System.EventHandler(this.ConvertToAnime_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox imageAddress;
+        private System.Windows.Forms.Button selectImageButton;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label imageAddressLabel;
+        private System.Windows.Forms.Label saveLabel;
+        private System.Windows.Forms.TextBox saveAddress;
+        private System.Windows.Forms.Button selectFolderButton;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.Label CopyrightLabel;
+        private System.Windows.Forms.RadioButton typeConvert1;
+        private System.Windows.Forms.RadioButton typeConvert2;
+    }
+}
+
---
+++ Convert to Anime/Main.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Drawing;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Convert_to_Anime
+{
+    public partial class ConvertToAnime : Form
+    {
+        private Thread convert;
+        public ConvertToAnime()
+        {
+            InitializeComponent();
+        }
+
+        public string version = "1.0.0";
+
+        private void selectImage(object sender, EventArgs e)
+        {
+            OpenFileDialog imageDialog = new OpenFileDialog();
+
+            imageDialog.InitialDirectory = "Pictures";
+            imageDialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.bmp, *.png) | *.jpg; *.jpeg; *.jpe; *.bmp; *.png";
+            imageDialog.FilterIndex = 2;
+            imageDialog.RestoreDirectory = true;
+
+            if (imageDialog.ShowDialog() == DialogResult.OK)
+            {
+                imageAddress.Text = imageDialog.FileName;
+                convertButton.Enabled = true;
+                Status.Text = "Trạng thái: Vui lòng nhấn Convert để bắt đầu tạo hình ảnh";
+            }
+        }
+
+        private void convertImage(object sender, EventArgs e)
+        {
+            convert = new Thread(new ThreadStart(convertImageThread));
+            if (convertButton.Text == "Convert")
+            {
+                convert.Start();
+
+                convertButton.Text = "Stop";
+
+                typeConvert1.Enabled = false;
+                typeConvert2.Enabled = false;
+
+                selectImageButton.Enabled = false;
+                selectFolderButton.Enabled = false;
+
+                Status.Text = "Trạng thái: Đang thực hiện tác vụ, vui lòng chờ...";
+            }
+            else
+            {
+                convert.Abort();
+
+                convertButton.Text = "Convert";
+
+                typeConvert1.Enabled = true;
+                typeConvert2.Enabled = true;
+
+                selectImageButton.Enabled = true;
+                selectFolderButton.Enabled = true;
+
+                Status.Text = "Trạng thái: Tác vụ đã bị hủy bỏ, vui lòng nhấn nút Convert nếu muốn thực hiện lại!";
+            }
+        }
+
+        private void convertStatus(bool status)
+        {
+            if(status)
+            {
+                convertButton.Invoke(new Action(() => convertButton.Text = "Convert"));
+
+                Status.Invoke(new Action(() => Status.Text = "Trạng thái: Tạo ảnh thành công, ảnh đã được lưu vào thư mục chỉ định!"));
+
+                typeConvert1.Invoke(new Action(() => typeConvert1.Enabled = true));
+                typeConvert2.Invoke(new Action(() => typeConvert2.Enabled = true));
+
+                selectImageButton.Invoke(new Action(() => selectImageButton.Enabled = true));
+                selectFolderButton.Invoke(new Action(() => selectFolderButton.Enabled = true));
+            }
+        }
+
+        public void convertImageThread()
+        {
+            int numOutput, t = (typeConvert1.Checked) ? 0 : 1;
+            bool s = (imageAddress.Text == "") ? false : true;
+
+            if (s)
+            {
+                while (true)
+                {
+                    if (!convert.IsAlive)
+                        break;
+
+                    if (File.Exists(imageAddress.Text))
+                    {
+                        byte[] imageArray = File.ReadAllBytes(@"" + imageAddress.Text);
+                        string encodeImage = Convert.ToBase64String(imageArray);
+
+                        string URI = "https://photoshopvn.com/api/socket/convert-raw-text.php?mode=" + t;
+                        string upload = "image=" + encodeImage;
+
+                        using (WebClient wc = new WebClient())
+                        {
+                            try
+                            {
+                                //Send data to URI
+                                wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+                                string HtmlResult = wc.UploadString(URI, upload);
+
+                                //Read numOutput value
+                                if (File.Exists(@".\data.ini"))
+                                {
+                                    string[] readData = File.ReadAllText(@".\data.ini").Split(new char[] { '\n' });
+                                    string[] numData = readData[1].Split(new char[] { '=' });
+                                    numOutput = Convert.ToInt32(numData[1]);
+                                }
+                                else
+                                {
+                                    writeDataFile(0, @"C:\");
+
+                                    numOutput = 0;
+                                }
+
+                                //Handling response data
+                                if (HtmlResult.Length > 100)
+                                {
+                                    if (convert.IsAlive == true)
+                                    {
+                                        LoadImage(HtmlResult, numOutput);
+                                        convertStatus(true);
+                                        convert.Abort();
+                                    }
+                                }
+                            }
+                            catch (Exception e) { }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void LoadImage(string base64code, int numOutput)
+        {
+            byte[] bytes = Convert.FromBase64String(base64code);
+
+            string saveToAddress = saveAddress.Text + @"\" + (numOutput + 1) + ".png";
+
+            using (Image image = Image.FromStream(new MemoryStream(bytes)))
+            {
+                image.Save(saveToAddress, ImageFormat.Png);
+
+                writeDataFile(numOutput + 1, saveAddress.Text);
+            }
+        }
+
+        private void selectSaveFolder(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    saveAddress.Text = fbd.SelectedPath;
+
+                    //Update new Dir
+                    string[] readData = File.ReadAllText(@".\data.ini").Split(new char[] { '\n' });
+                    string[] numData = readData[1].Split(new char[] { '=' });
+                    writeDataFile(Convert.ToInt32(numData[1]), saveAddress.Text);
+                }
+            }
+        }
+
+        private void ConvertToAnime_Load(object sender, EventArgs e)
+        {
+            bool appStatus;
+            string appVersion = new WebClient().DownloadString("https://photoshopvn.com/api/public/app/anime/version");
+
+            if (version == appVersion)
+                appStatus = true;
+            else
+                appStatus = false;
+
+            if(!appStatus)
+            {
+                MessageBox.Show("Phiên bản đã hết hạn, nếu có cập nhật xin hãy tải phiên bản mới để sử dụng!", "Lỗi khởi động", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+
+
+            if (!File.Exists(@".\data.ini"))
+            {
+                writeDataFile(0, @"C:\");
+            }
+            else if(File.ReadAllText(@".\data.ini") == "")
+            {
+                writeDataFile(0, @"C:\");
+            }
+
+            readFileData();
+        }
+
+        public void writeDataFile(int num, string Dir)
+        {
+            string Data = "[Convert to Anime]\n" +
+                          "number=" + num +"\n" +
+                          "SvtoDir=" + Dir;
+            File.WriteAllText(@".\data.ini", Data);
+        }
+
+        public void readFileData()
+        {
+            string[] readData = File.ReadAllText(@".\data.ini").Split(new char[] { '\n' });
+            if(readData.Length == 3)
+            {
+                string[] numData = readData[1].Split(new char[] { '=' });
+                string[] dỉrData = readData[2].Split(new char[] { '=' });
+
+                if (Directory.Exists(dỉrData[1]))
+                    saveAddress.Text = dỉrData[1];
+                else if (saveAddress.Text == "")
+                    saveAddress.Text = @"C:\";
+                else
+                    writeDataFile(0, @"C:\");
+
+
+                if (Convert.ToInt32(numData[1]) < 0)
+                    writeDataFile(0, saveAddress.Text);
+            }
+            else
+                writeDataFile(0, @"C:\");
+        }
+
+        private void copyrightAuthor(object sender, EventArgs e)
+        {
+            Process.Start("https://www.facebook.com/10002217682048");
+        }
+    }
+}
